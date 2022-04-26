4794163df485a03724730a33f1465f317cef4ba9
kishanpatel049 <99271404+kishanpatel049@users.noreply.github.com>
2022-04-26 08:43:46 -0400

    added home page buttons

--- SchoolManagement/src/app/home/home.component.ts
+++ SchoolManagement/src/app/home/home.component.ts
@@ -8,6 +8,43 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  links = [
+    {
+      route: "schools",
+      rouerName: "schools"
+
+    },
+    {
+      route: "classes",
+      rouerName: "classes"
+
+    },
+    {
+      route: "semesters",
+      rouerName: "semesters"
+
+    },
+    {
+      route: 'finance',
+      rouerName: "finance"
+
+    },
+    {
+      route: "register",
+      rouerName: "register"
+
+    },
+    {
+      route: "records",
+      rouerName: "records"
+
+    },
+    {
+      route: "roles",
+      rouerName: "roles"
+
+    }
+  ];
 
   ngOnInit(): void {
   }
