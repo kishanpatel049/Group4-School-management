730a0e5086641678548acdb1cf6f86b0addd5652
kishanpatel049 <99271404+kishanpatel049@users.noreply.github.com>
2022-04-25 23:30:19 -0400

    added example html

--- SchoolManagement/src/app/material/material.module.ts
+++ SchoolManagement/src/app/material/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button'
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 const MatterialComponenst = [
-  MatButtonModule
+  MatButtonModule,
+  MatSidenavModule
 ];
 
 
