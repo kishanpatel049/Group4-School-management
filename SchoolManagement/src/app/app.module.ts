7d4f8eb0706219c14c93cd257a5e6eab9a3827b3
Kadeh Ikpe <40577808+Kadehikpe14@users.noreply.github.com>
2022-04-27 20:05:26 -0400

    added module
    I tried to add the sidenav module but there keeps showing an error in the app.component.html

--- SchoolManagement/src/app/app.module.ts
+++ SchoolManagement/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatSidenavModule //the sidenav is not working in html
   ],
   providers: [],
   bootstrap: [AppComponent]
