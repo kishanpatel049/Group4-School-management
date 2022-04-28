301de022dbc1cb8181515b7b9c8f50260d10ae85
kishanpatel049 <99271404+kishanpatel049@users.noreply.github.com>
2022-04-28 15:24:24 -0400

    added more html

--- SchoolManagement/src/app/material/material.module.ts
+++ SchoolManagement/src/app/material/material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
 
 const MatterialComponenst = [
   MatButtonModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatMenuModule
 ];
 
 
