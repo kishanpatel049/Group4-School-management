b7d82b15bb1478fe7308d7bba814923f67b0b5b3
Kadeh Ikpe <40577808+Kadehikpe14@users.noreply.github.com>
2022-05-07 17:26:13 -0400

    input

--- SchoolManagement/src/app/schools/schools.module.ts
+++ SchoolManagement/src/app/schools/schools.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { SchoolsRoutingModule } from './schools-routing.module';
 import { SchoolsHomeComponent } from './schools-home/schools-home.component';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+
 
 @NgModule({
   declarations: [
